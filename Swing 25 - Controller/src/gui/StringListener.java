cac8025a1792788b8673130c0110981402ed63c9
Rick Nie <1653836798121002@mil>
2026-07-14 16:16:38 -0700

    Swing 25

--- Swing 25 - Controller/src/gui/StringListener.java
+++ Swing 25 - Controller/src/gui/StringListener.java
@@ -1,4 +1,4 @@
-//implementation receives string
+package gui;//implementation receives string
 
 public interface StringListener {
     public void textEmitted(String text);
---
+++ Swing24 - Data Model/src/gui/StringListener.java
@@ -0,0 +1,5 @@
+package gui;//implementation receives string
+
+public interface StringListener {
+    public void textEmitted(String text);
+}
