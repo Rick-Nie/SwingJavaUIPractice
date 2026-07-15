d111e668f37a72907b8d8fb10a554849dd93bec1
Rick Nie <1653836798121002@mil>
2026-07-15 16:41:40 -0700

    Swing 36 no bueno

--- Swing36 - Connecting to Databases/src/gui/PrefsListener.java
+++
@@ -1,5 +0,0 @@
-package gui;
-
-public interface PrefsListener {
-    public void preferencesSet(String user, String password, int port);
-}
--- Swing36 - Connecting to Databases/src/gui/StringListener.java
+++
@@ -1,5 +0,0 @@
-package gui;//implementation receives string
-
-public interface StringListener {
-    public void textEmitted(String text);
-}
