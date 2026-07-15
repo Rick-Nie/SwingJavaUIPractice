87fa9350189cf51bc53472fc5b3306e5f828a767
Rick Nie <1653836798121002@mil>
2026-07-15 13:10:41 -0700

    Swing 32

---
+++ Swing32 - Spinners/src/gui/PrefsDialog.java
@@ -0,0 +1,12 @@
+package gui;
+
+import javax.swing.*;
+
+public class PrefsDialog extends JDialog {
+
+    public PrefsDialog(JFrame parent){
+        super(parent, "Preferences", false);
+
+        setSize(400,300);
+    }
+}
