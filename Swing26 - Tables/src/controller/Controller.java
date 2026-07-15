564b2796923713d5d7d721202a229d70a172bc34
Rick Nie <1653836798121002@mil>
2026-07-15 08:18:38 -0700

    Swing 26

---
+++ Swing26 - Tables/src/controller/Controller.java
@@ -0,0 +1,57 @@
+package controller;
+
+import gui.FormEvent;
+import model.*;
+
+public class Controller {
+    DataBase db = new DataBase();
+
+    public void addPerson(FormEvent ev){
+        String name = ev.getName();
+        String occupation = ev.getOccupation();
+        int ageCat = ev.getAgeCategory();
+        String empCat = ev.getEmploymentCategory();
+        boolean isUs = ev.isUsCitizen();
+        String taxId = ev.getTaxId();
+        String gender = ev.getGender();
+
+        AgeCategory ageCategory = null;
+
+        switch (ageCat) {
+            case 0:
+                ageCategory = AgeCategory.child;
+                break;
+            case 1:
+                ageCategory = AgeCategory.adult;
+                break;
+            case 2:
+                ageCategory = AgeCategory.senior;
+                break;
+        }
+
+        EmploymentCategory employmentCategory;
+
+        if (empCat.equals("employed")) {
+            employmentCategory = EmploymentCategory.employed;
+        } else if (empCat.equals("employed")) {
+            employmentCategory = EmploymentCategory.selfEmployed;
+        } else if (empCat.equals("employed")) {
+            employmentCategory = EmploymentCategory.unemployed;
+        } else{
+            employmentCategory = EmploymentCategory.other;
+            System.err.println(empCat);
+        }
+
+        Gender genderCat;
+
+        if (gender.equals("male")){
+            genderCat = Gender.male;
+        } else{
+            genderCat = Gender.female;
+        }
+
+        Person person = new Person(name, occupation, ageCategory, employmentCategory, taxId, isUs, genderCat);
+
+        db.addPerson(person);
+    }
+}
